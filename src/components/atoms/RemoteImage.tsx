07aee2ab779bc6353e78a7270d9bc3a4c638ebe1
Anil Thakur <macbookpro@Anils-MacBook-Pro.local>
2024-03-21 00:35:54 +0530

    inital code

---
+++ src/components/atoms/RemoteImage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Image as RNImage} from 'react-native';
+import type {ImageProps} from 'react-native';
+
+const RemoteImage: React.FC<ImageProps> = ({...rest}) => <RNImage {...rest} />;
+export default React.memo(RemoteImage);
---
+++ src/navigations/navigationProps.ts
@@ -0,0 +1,9 @@
+import type {NativeStackScreenProps} from '@react-navigation/native-stack';
+
+export type ApplicationStackParamList = {
+  Startup: undefined;
+  Example: undefined;
+};
+
+export type ApplicationScreenProps =
+  NativeStackScreenProps<ApplicationStackParamList>;
